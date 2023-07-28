1d09084324847fee1797ffc7e740d1bf9587e171
joehaddad94 <127773834+joehaddad94@users.noreply.github.com>
2023-07-29 01:16:53 +0300

    Added all necessary files.

---
+++ eMarket-frontend/script/script.js
@@ -0,0 +1,27 @@
+const pages = {}
+
+pages.base_url = "http://localhost/ClassroomClone/back-end/";
+
+// Common Functions
+
+pages.getAPI = async(url) => {
+    try {
+        return await axios(url)
+    } catch (error) {
+        pages.print_message("Error from GET API: " + error)
+    }
+}
+
+pages.postAPI = async(api_url, api_data) => {
+    try {
+        return await axios.post(api_url, api_data);
+    } catch (error) {
+        pages.print_message("Error from Linking (POST)" + error)
+    }
+}
+
+pages.loadFor = (page) => {
+    eval("pages.page_" + page + "();")
+}
+
+pages.page_index = () => {}
