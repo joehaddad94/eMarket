9e1971a4186ce3ca98e1cdf0209d5107ff95e44d
joehaddad94 <127773834+joehaddad94@users.noreply.github.com>
2023-07-30 15:26:07 +0300

    Added Category APIs

---
+++ eMarket-backend/app/Http/Controllers/CategoriesController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Category;
+
+class CategoriesController extends Controller
+{
+   function addOrUpdateCategory(Request $request, $id = "add") {
+    if($id == "add") {
+        $category = new Category;
+    } else {
+        $category = Category::find($id);
+    }
+
+    $category->name = $request->name ? $request->name : $article->name;
+    $category->save();
+
+    return json_encode(["categories" => $category]);
+   }
+
+   function deleteCategory($id) {
+    $category = Category::find($id);
+    $category->delete();
+
+    return response()->json(['message' => 'Category deleted successfully']);
+    
+   }
+}
--- eMarket-backend/routes/api.php
+++ eMarket-backend/routes/api.php
@@ -1,7 +1,11 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
+
 use App\Http\Controllers\API\AuthController;
+use App\Http\Controllers\CategoriesController;
 
 Route::controller(AuthController::class)->group(function () {
     Route::post('login', 'login');
@@ -9,3 +13,6 @@ Route::controller(AuthController::class)->group(function () {
     Route::post('logout', 'logout');
     Route::post('refresh', 'refresh');
 });
+
+Route::post('/add_update_category', [CategoriesController::class, "addOrUpdateCategory"]);
+Route::post('/delete_category/{id}', [CategoriesController::class, "deleteCategory"]);
