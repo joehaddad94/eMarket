dbe21d6c77bd8dc37db482f71e1b32969e2560dc
joehaddad94 <127773834+joehaddad94@users.noreply.github.com>
2023-07-31 01:02:37 +0300

    Added sign up and sign in functionalities

--- eMarket-backend/app/Http/Controllers/API/AuthController.php
+++ eMarket-backend/app/Http/Controllers/API/AuthController.php
@@ -52,6 +52,7 @@ class AuthController extends Controller
             'name' => $request->name,
             'email' => $request->email,
             'password' => Hash::make($request->password),
+            'type_id' => $request->type_id,
         ]);
 
         return response()->json([
